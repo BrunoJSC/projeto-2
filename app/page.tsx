cda39a25bada10dea296cf70877c46f93adfacef
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-10-24 20:44:21 -0300

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex bg-[#000] min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex bg-[#000] min-h-screen flex-col items-center p-24 ">
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,31 +1,41 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export function Hero() {
   return (
-    <section className="flex items-center">
-      <div>
+    <section className="flex flex-col md:flex-row items-center ">
+      <div className=" w-full md:w-1/2">
         <Image
           src="/person.png"
           alt="Logo"
-          className="object-contain w-[542px] h-[518px]"
+          className="object-contain w-full h-full"
           width={542}
           height={518}
         />
       </div>
 
-      <div>
-        <h1 className="text-4xl font-bold text-white">
+      <div className="w-full md:w-1/2 p-8">
+        <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
           Matheus
           <br />
           <span className="text-[#00FFBD]">Damasceno</span>
         </h1>
 
-        <p className="text-white w-[628px] text-[20px] font-medium">
+        <div className="bg-[#00FFBD] mb-4 h-1 w-24 md:w-32" />
+
+        <p className="text-white text-base md:text-lg font-medium mb-4">
           Minha busca constante por ajudar as pessoas me permite desvendar as
           sutilezas das necessidades humanas e traduzi-las em soluções de design
           que complementam e apoiam os objetivos de negócios de forma autêntica
           e impactante.
         </p>
+
+        <Link
+          href="/sobre"
+          className="py-3 px-6 md:py-4 md:px-8 border border-[#00FFBD] text-[#00FFBD] inline-block"
+        >
+          Saiba mais
+        </Link>
       </div>
     </section>
   );
