c9c331ae1ec70f550f20ac7150b60b15c94cf42a
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-10-25 02:04:44 -0300

    ....

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex bg-[#000] min-h-screen flex-col items-center p-24 ">
+    <main className="flex bg-[#000] min-h-screen flex-col items-center">
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,30 +1,20 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export function Hero() {
   return (
-    <section className="w-full">
-      <div className="flex items-center  flex-col md:flex-row">
-        <div className="w-full md:w-[920] bg-[url('/person.png')] bg-cover bg-no-repeat h-[542px]">
-          {/* <Image
-          src="/person.png"
-          alt="Logo"
-          className="object-contain w-full h-full"
-          width={542}
-          height={518}
-        /> */}
-        </div>
+    <section className="w-full flex flex-col justify-center min-h-screen relative ">
+      <div className="w-full flex  justify-center  px-4">
+        <div className="w-full hidden md:block h-[918px] md:w-[946px] md:bg-[url('/person.png')] bg-no-repeat -z-[1px]" />
 
-        <div className="w-full md:w-[calc(100% - 920px)] p-8 ">
-          <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
+        <div className="ml-0 md:-ml-56 md:max-w-[628px] h-[380px] md:mt-44">
+          <h1 className="text-4xl md:text-6xl font-bold text-white">
             Matheus
-            <br />
-            <span className="text-[#00FFBD]">Damasceno</span>
+            <p className="text-[#00FFBD] ">Damasceno</p>
           </h1>
 
-          <div className="bg-[#00FFBD] mb-4 h-1 w-24 md:w-32" />
+          <div className="w-[120px] border-4 border-[#00FFBD] my-4" />
 
-          <p className="w-full md:w-[628px]  text-white text-base md:text-lg font-medium mb-4 leading-relaxed">
+          <p className="text-white md:w-full text-[20px] font-medium">
             Minha busca constante por ajudar as pessoas me permite desvendar as
             sutilezas das necessidades humanas e traduzi-las em soluções de
             design que complementam e apoiam os objetivos de negócios de forma
@@ -33,11 +23,14 @@ export function Hero() {
 
           <Link
             href="/sobre"
-            className="py-3 px-6 md:py-4 md:px-8 border border-[#00FFBD] text-[#00FFBD] inline-block"
+            className="text-white border-4 border-[#00FFBD] py-[16px] px-[56px] inline-block uppercase font-bold mt-5"
           >
             Saiba mais
           </Link>
         </div>
+        <p className="text-white text-center z-10 absolute bottom-0 left-0 right-0 text-[#9E9E9E]">
+          Product Design / UX Research / UI Design / Design Thinking
+        </p>
       </div>
     </section>
   );
